0e8e0926e07d961d7fd159ba748b27995722fd2c
Pritesh Kadiwala <pkadiwal@purdue.edu>
2017-10-23 20:10:36 -0400

    vuetify dropdown

--- src/components/header.vue
+++ src/components/header.vue
@@ -4,23 +4,33 @@
       <v-list>
         <v-list-tile @click="">
           <v-list-tile-action>
-            <v-icon>account_box</v-icon>
+            <v-icon class="ma-1">account_box</v-icon>
           </v-list-tile-action>
-          <v-list-tile-content>
+          <v-list-tile-content class="ma-0">
             Account
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar>
+    <v-toolbar dark class="indigo">
       <v-toolbar-side-icon @click.stop="sideNav = !sideNav" class="hidden-sm-and-up"></v-toolbar-side-icon>
       <v-toolbar-title>Where@</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-xs-only">
-        <v-btn flat>
-          <v-icon left>account_box</v-icon>
-          Account
-        </v-btn>
+        <v-menu open-on-hover offset-y>
+          <v-btn flat slot="activator">
+            <v-icon left class="ma-1">account_box</v-icon>
+            Account
+          </v-btn>
+          <v-list>
+            <v-list-tile @click="">
+              <v-list-tile-title>Sign in</v-list-tile-title>
+            </v-list-tile>
+            <v-list-tile @click="">
+              <v-list-tile-title>Register</v-list-tile-title>
+            </v-list-tile>
+          </v-list>
+        </v-menu>
       </v-toolbar-items>
     </v-toolbar>
   </v-app>
@@ -31,13 +41,17 @@
 
     data () {
       return {
-        sideNav: false
+        sideNav: false,
+        menuItems: [
+          { icon: 'account_box', title: 'Account' },
+
+        ]
       }
     }
   }
 
 </script>
 
-<style scoped>
-
+<style lang="stylus">
+  @import '../stylus/main'
 </style>
