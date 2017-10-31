02a2616cffd4d657f208939ee8c72878e5e3fa72
cory <claker@purdue.edu>
2017-10-31 18:41:45 -0400

    made donate page more interesting

--- src/components/donate.vue
+++ src/components/donate.vue
@@ -1,11 +1,15 @@
 <template>
 	<div>
-		<div>
+		<div v-show="show===false">
+			<h3>Thanks for the money!!!</h3>
+		</div>
+		<div v-show="show===true" >
 			<PayPal
 				amount="1.00"
 				currency="USD"
 				:dev="true"
 				:client='credentials'
+				v-on:paypal-paymentCompleted="changeBool($event)"
 				></PayPal>
 		</div>
 	</div>
@@ -21,13 +25,24 @@ export default {
     	credentials: {
     		sandbox: "AaabrPP3UWw--QGiWf582u1guSIUjx0ItPqm1ocI26OEZ4E5SiM1DwC_HsXzsF8vEkw2-qbMdchCbnNr",
     		production: "AdtCNixFoW4ZzTOf0g_D4986xLwR9ZvD17JU2Y9LHFA4YazJQvXlY9AFlYbgUxnakV36Eh4_J_Bwybev",
- 		}
+ 		},
+ 		show: true,
     }
   },
-
+  methods: {
+  	changeBool($event) {
+  		console.log($event);
+  		if (this.show == true) {
+              this.show = false;
+            }
+            else {
+              this.show = true;
+            }
+  	}	
+  },
   components: {
   	PayPal
-  }
+  },
 }
 
 </script>
